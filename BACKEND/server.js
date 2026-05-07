4cff01890c07229878fa00335dea9ee02abd2bec
rithish2413 <24ucs230@lnmiit.ac.in>
2026-05-07 19:05:37 +0530

    updated project

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -4,10 +4,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("REQ:", req.method, req.url);
-  next();
-});
+
 app.use(express.json());
 const path = require('path');
 
